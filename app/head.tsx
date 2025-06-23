8f55348e97d6bad009e0a05ba85d6472425174c4
fahrul <kacomakassar@gmail.com>
2025-06-23 20:48:02 +0800

    add fc:frame meta tag for Farcaster Mini App

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Mini App Placeholder</title>
+      <meta name="fc:frame" content='{"version":"next","imageUrl":"https://placehold.co/600x400","button":{"title":"Mulai","action":{"type":"launch_frame","url":"https://farcaster-miniapp-coral.vercel.app"}}}' />
+    </>
+  );
+}
