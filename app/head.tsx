0e0a5ab1f684e0bdb958af781dae236413a35188
fahrul <kacomakassar@gmail.com>
2025-06-23 21:12:21 +0800

    fix: add fc:frame meta tag

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,8 @@
 export default function Head() {
   return (
     <>
-      <title>Mini App Placeholder</title>
+      <title>Mini App taik Placeholder</title>
       <meta name="fc:frame" content='{"version":"next","imageUrl":"https://placehold.co/600x400","button":{"title":"Mulai","action":{"type":"launch_frame","url":"https://farcaster-miniapp-coral.vercel.app"}}}' />
     </>
   );
-}
+}   
